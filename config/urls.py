97d1e9bbae95da1f74f3e07c18c7c933e523143f
Jgaray3008 <110505188+Jgaray3008@users.noreply.github.com>
2024-07-27 19:50:50 -0600

    Gestion de usuarios
    Se agregan las vistas de gestion de usuarios, create, read, update y delete

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
-
-import Usuarios
-import Usuarios.views
+from Usuarios import views as usuarios_views
 from . import views
 
 urlpatterns = [
-    path('', Usuarios.views.external_login, name='external_login'),
+    path('', usuarios_views.external_login, name='external_login'),
     path('index/', views.index, name="index"),
     path('admin/', admin.site.urls),
     path('terminal/', views.pos_view, name='pos_view'),
-    path('base2/', views.base2, name='base2'),
-    path('', include("django.contrib.auth.urls")),
-    
+    path('accounts/', include("django.contrib.auth.urls")),
+    path('usuarios/', include('Usuarios.urls')),
 ]
--- config/views.py
+++ config/views.py
@@ -6,5 +6,3 @@ def index(request):
 def pos_view(request):
     return render(request, 'Terminal.html')
 
-def base2(request):
-    return render(request, 'base2.html')
