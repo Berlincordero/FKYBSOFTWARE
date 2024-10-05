e0cc84ca9a41fe4c919fa9aafa32540f74503ea9
Jgaray3008 <110505188+Jgaray3008@users.noreply.github.com>
2024-10-05 02:31:42 -0600

    Se agrega login y script db

--- Usuarios/models.py
+++ Usuarios/models.py
@@ -3,3 +3,12 @@ from django.db import models
 
 
 # Create your models here.
+
+from django.db.models.signals import post_migrate
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+
+@receiver(post_migrate)
+def create_default_admin(sender, **kwargs):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
+
 
 urlpatterns = [
     
+    path('external_login/', views.external_login, name='external_login'),
     path('', views.usuarios, name='usuarios'),
+    path('logout/', views.logout_view, name='logout'),
     
 ]
