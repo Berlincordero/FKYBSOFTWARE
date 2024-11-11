43b3fe1b79daad329580f705241bd61dcdb3d091
Jgaray3008 <110505188+Jgaray3008@users.noreply.github.com>
2024-11-11 02:09:58 -0600

    fix app clientes

--- Clientes/urls.py
+++ Clientes/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = 'clientes'
-
 urlpatterns = [
     path('', views.modulo_clientes, name='modulo_clientes'),
     path('eliminar_cliente/<int:id_cliente>/', views.eliminar_cliente, name='eliminar_cliente'),
     path('agregar_cliente/', views.agregar_cliente, name='agregar_cliente'),
     path('editar_cliente/<str:id_cliente>/', views.editar_cliente, name='editar_cliente'),  # Ruta para editar cliente
-
 ]
