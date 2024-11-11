863b275009ceaf3c7010c9613f58755ccfbc6fd1
Jgaray3008 <110505188+Jgaray3008@users.noreply.github.com>
2024-11-11 01:40:32 -0600

    Delete clientes directory

--- clientes/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Cliente
-
-class ClienteForm(forms.ModelForm):
-    class Meta:
-        model = Cliente
-        fields = ['id_cliente', 'nombre', 'primer_apellido', 'segundo_apellido', 'provincia', 'canton', 'distrito', 'direccion_exacta', 'telefono', 'correo']
