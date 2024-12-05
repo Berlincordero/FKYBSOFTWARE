3ab79b695a08956c084bbcdbadae9cc4c420c1dc
Berlincordero <114275282+Berlincordero@users.noreply.github.com>
2024-12-05 05:07:42 -0600

    caja registradora 20%
    se realiza un 20% de las funciones del modulo

--- Cajaregistradora/models.py
+++ Cajaregistradora/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-
-
 class Factura(models.Model):
-    fecha = models.DateTimeField()
-    numero_factura = models.CharField(max_length=50, unique=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+    numero_factura = models.CharField(max_length=50, unique=True, blank=True)
     cliente = models.CharField(max_length=100, blank=True, null=True)
     codigo = models.CharField(max_length=50)
     nombre = models.CharField(max_length=50)
@@ -13,11 +11,16 @@ class Factura(models.Model):
     precio_venta = models.DecimalField(max_digits=10, decimal_places=2)
     iva = models.DecimalField(max_digits=10, decimal_places=2)
     total = models.DecimalField(max_digits=10, decimal_places=2)
-    
+
+    def save(self, *args, **kwargs):
+        if not self.numero_factura:
+            # Obtiene la última factura para determinar el número siguiente
+            last_factura = Factura.objects.last()
+            new_number = 0 if not last_factura else int(last_factura.numero_factura.split()[1]) + 1
+            self.numero_factura = f"F {new_number}"
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f"Factura #{self.numero_factura} - Total: {self.total}"
-    
-    
-
     
     
--- Cajaregistradora/views.py
+++ Cajaregistradora/views.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.shortcuts import render
+from .models import Factura
+from datetime import datetime
 
-# Create your views here.
+
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
@@ -11,7 +13,7 @@ def get_client_ip(request):
     
     # Ajustar para entorno local
     if ip == '127.0.0.1':
-        ip = '192.168.0.101'  # IP unica de terminal local
+        ip = '192.168.0.101'  # IP única de terminal local
     return ip
 
 def Cajaregistradora_view(request):
@@ -27,6 +29,24 @@ def Cajaregistradora_view(request):
                 terminal = term
                 break
 
-    return render(request, 'Cajaregistradora.html', {'terminal': terminal, 'sucursal': sucursal})
+    # Crear una factura para prueba
+    factura = Factura.objects.create(
+        cliente="Cliente de Prueba",
+        codigo="PRUEBA001",
+        nombre="Producto de Prueba",
+        descripcion="Una descripción breve",
+        cantidad=1,
+        precio_venta=100.00,
+        iva=13.00,
+        total=113.00
+    )
 
+    # Obtener la fecha actual
+    fecha_actual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+    return render(request, 'Cajaregistradora.html', {
+        'terminal': terminal,
+        'sucursal': sucursal,
+        'numero_factura': factura.numero_factura,
+        'fecha_actual': fecha_actual,  # Enviar la fecha al contexto
+    })
